119aa24f2d2d0e39b4b3f69c100b44a2fbde0b54
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-02-04 22:29:53 +0000

    home role added /BB

---
+++ ansible/roles/home/tasks/create_dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -i
+set -u
+
+mkdir -p ~/bin
+mkdir -p ~/files
+mkdir -p ~/files/eclipse_workspaces
+mkdir -p ~/projects
+mkdir -p ~/progs
+mkdir -p ~/encrypted
+mkdir -p ~/Downloads
+
