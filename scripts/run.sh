07f351649c71f7c31a3526a980132db53c1ccb45
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-02-05 02:46:56 +0100

    role user added /BB

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,6 @@ OLD_DIR=$( pwd )
 CM_DIR=$( readlink -f ${0%/*/*} )
 
 cd ${CM_DIR}/ansible
-${CM_DIR}/.venv/bin/ansible-playbook -i hosts -c local playbook.yml
+${CM_DIR}/.venv/bin/ansible-playbook -i hosts -c local playbook.yml -K
 
 cd "$OLD_DIR"
