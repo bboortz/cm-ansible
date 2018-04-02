4683ca37d4df5f4d40a4c4c712c0ff22c2b78b68
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2018-04-02 14:11:23 +0200

    test scripts updated /BB

--- bin/docker_entry_test.sh
+++ bin/docker_entry_test.sh
@@ -19,3 +19,4 @@ ROOTDIR="${CURDIR%/*}"
 ${ROOTDIR}/bin/install_dependencies.sh 
 CM_DRYRUN=1 ${ROOTDIR}/cm.sh deploy
 ${ROOTDIR}/cm.sh deploy
+exit 0
--- bin/test_local.sh
+++ bin/test_local.sh
@@ -15,8 +15,17 @@ ROOTDIR="${CURDIR%/*}"
 #
 # program
 #
+
+# test all roles
+for role in ${ROOTDIR}/ansible/roles/*; do
+	ANSIBLE_INVENTORY="/home/benni/projects/cm-ansible/ansible/hosts" ANSIBLE_ROLES_PATH="/home/benni/projects/cm-ansible/ansible/roles" ansible-playbook -vvvvvvv  -c local ${role}/tests/test.yml
+done
+
+# test local deploy
 sudo CM_DRYRUN=1 ./cm.sh deploy
 sudo ./cm.sh deploy
+
+# run test in different docker container
 CM_DOCKER_IMAGE=python:2 ${ROOTDIR}/cm.sh test
 CM_DOCKER_IMAGE=ubuntu:16.04 ${ROOTDIR}/cm.sh test
 CM_DOCKER_IMAGE=debian:latest ${ROOTDIR}/cm.sh test
