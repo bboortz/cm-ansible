105601111ea8977cb3ea27d9fe026d0ccc91c19a
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2018-04-02 06:48:30 +0200

    using a script for testing with travis /BB

---
+++ bin/test_travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+
+DOCKER_IMAGE="${DOCKER_IMAGE:-python:2}"
+
+echo
+echo "INFO: testing in docker image: ${DOCKER_IMAGE}"
+
+docker run -t -v $(pwd):/code "${DOCKER_IMAGE}" /bin/bash -c '/code/bin/install_dependencies.sh && /code/cm.sh'
