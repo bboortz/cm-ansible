d1e455da4269939c9d507209ed61dc3a371e4b18
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2018-04-08 11:53:34 +0200

    cm.sh test added to travis /BB

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -8,4 +8,4 @@ CM_DOCKER_IMAGE="${CM_DOCKER_IMAGE:-python:2}"
 echo
 echo "INFO: testing in docker image: ${CM_DOCKER_IMAGE}"
 
-docker run -t -v $(pwd):/code "${CM_DOCKER_IMAGE}" /bin/bash -c '/code/bin/install_dependencies.sh && CM_DRYRUN=1 /code/cm.sh deploy && /code/cm.sh deploy'
+docker run -t -v $(pwd):/code "${CM_DOCKER_IMAGE}" /bin/bash -c '/code/bin/install_dependencies.sh && /code/cm.sh test && CM_DRYRUN=1 /code/cm.sh deploy && /code/cm.sh deploy'
